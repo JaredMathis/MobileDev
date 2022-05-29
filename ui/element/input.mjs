84b34ed54ee10277d256414fc0c1fb4c18bc8042
JaredMathis <jared.mathis@native.consulting>
2022-05-28 22:16:21 -0400

    refactor

--- ui/element/input.mjs
+++ ui/element/input.mjs
@@ -1,7 +1,11 @@
 export function ui_element_input(parent, placeholder) {
-    let input = document.createElement('input');
-    parent.appendChild(input)
-
+    let input = ui_element(parent, 'input');
     input.placeholder = placeholder;
     return input;
+}
+
+function ui_element(parent, tag_name) {
+    let e = document.createElement(tag_name);
+    parent.appendChild(e);
+    return e;
 }
