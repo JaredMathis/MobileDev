9729617559b465b824f46d8b091b8018c2eeacbc
JaredMathis <jared.mathis@native.consulting>
2022-05-28 22:21:30 -0400

    there should be a login button

--- app/component/main.mjs
+++ app/component/main.mjs
@@ -1,6 +1,11 @@
+import { ui_element } from "../../ui/element.mjs";
+import { ui_element_html_inner_set } from "../../ui/element/html/inner/set.mjs";
 import { ui_element_input } from "../../ui/element/input.mjs"
 
 export function app_component_main(parent) {
     let input = ui_element_input(parent, 'GitHub Personal access token');
     input.focus();
+
+    let button = ui_element(parent, 'button');
+    ui_element_html_inner_set(button, 'Login');
 }
