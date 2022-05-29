62b444d961124f28f1dadfce011517e3b62affa4
JaredMathis <jared.mathis@native.consulting>
2022-05-28 22:12:42 -0400

    Input should have placeholder value

--- app/component/main.mjs
+++ app/component/main.mjs
@@ -1,5 +1,5 @@
 import { ui_element_input } from "../../ui/element/input.mjs"
 
 export function app_component_main(parent) {
-    ui_element_input(parent);
+    ui_element_input(parent, 'GitHub Personal access token');
 }
--- ui/element/input.mjs
+++ ui/element/input.mjs
@@ -1,4 +1,6 @@
-export function ui_element_input(parent) {
+export function ui_element_input(parent, placeholder) {
     let input = document.createElement('input');
     parent.appendChild(input)
+
+    input.placeholder = placeholder;
 }
