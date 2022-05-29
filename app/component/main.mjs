e624e9e3ba4bed9d5ce46b11230996db47ea8a50
JaredMathis <jared.mathis@native.consulting>
2022-05-28 22:17:00 -0400

    input should be focused on page load

--- app/component/main.mjs
+++ app/component/main.mjs
@@ -2,4 +2,5 @@ import { ui_element_input } from "../../ui/element/input.mjs"
 
 export function app_component_main(parent) {
     let input = ui_element_input(parent, 'GitHub Personal access token');
+    input.focus();
 }
