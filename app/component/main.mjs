d77d16e48b06cf9eebee3b93ca45e69ab9f19451
JaredMathis <jared.mathis@native.consulting>
2022-05-28 22:37:15 -0400

    progress

--- app/component/main.mjs
+++ app/component/main.mjs
@@ -8,15 +8,30 @@ export function app_component_main(parent) {
     input.focus();
 
     let button = ui_element_button(parent, 'Login');
+
+    let login_message = ui_element_div(parent);
+
     button.addEventListener('click', async () => {
         let octokit = new Octokit({ auth: input.value });
         let {
             data: { login },
         } = await octokit.rest.users.getAuthenticated();
 
+        ui_element_html_inner_set(login_message, `Logged in as ${login}`);
+
         let repos = await octokit.rest.repos.listForAuthenticatedUser();
         console.log(repos)
     });
+}
+
+function ui_element_span(parent) {
+    let button = ui_element(parent, 'span');
+    return button;
+}
+
+function ui_element_div(parent) {
+    let button = ui_element(parent, 'div');
+    return button;
 }
 
 function ui_element_button(parent, button_text) {
